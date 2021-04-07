38a950bccf7fa80f7da0d1a6425a5afb1feb7dab
Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2021-04-06 19:45:22 -0500

    Add files via upload

--- math_convert.py
+++ math_convert.py
@@ -1,131 +1,44 @@
 # number conversion
-# zio800
-
-
-# decimal to binary
-def nc10_2(num10):
-    num2 = []
-    while num10 / 2 != 0:
-        num2.append(str(num10 % 2))
-        num10 = num10 // 2
-    num2.reverse()
-    ans = int(''.join(num2))
-    print('binary = ', ans)
-
-
-# decimal to octal
-def nc10_8(num10):
-    num8 = []
-    while num10 / 2 != 0:
-        num8.append(str(num10 % 8))
-        num10 = num10 // 8
-    num8.reverse()
-    ans = int(''.join(num8))
-    print('octal = ', ans)
-
-
-# decimal to hexadecimal
-def nc10_16(num10):
-    hexd = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
-    num16 = []
-    while num10 / 16 != 0:
-        num16.append(str(num10 % 16))
-        num10 = num10 // 16
-    num16.reverse()
-    for item in hexd:
-        num16 = [hexd[item] if x == item else x for x in num16]
-    ans = ''.join(num16)
-    print('hexadecimal = ', ans)
-
-
-# binary to decimal
-def nc2_10(num2):
-    lenn = [item for item in str(num2)]
-    lenn.reverse()
-    num10 = []
-    count = 0
-    for i in lenn:
-        num10.append(int(i) * (2 ** count))
-        count += 1
-    ans = sum(num10)
-    print('decimal = ', ans)
-
-
-# octal to decimal
-def nc8_10(num8):
-    lenn = [item for item in str(num8)]
-    lenn.reverse()
-    num10 = []
-    count = 0
-    for i in lenn:
-        num10.append(int(i) * (8 ** count))
-        count += 1
-    ans = sum(num10)
-    print('decimal = ', ans)
-
-
-# hexadecimal to decimal
-def nc16_10(num16):
-    hexd = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
-    lenn = [item for item in str(num16)]
-    lenn.reverse()
-    lenn2 = []
-    for item in hexd:
-        lenn2 = [item if x == hexd[item] else x for x in lenn]
-    num10 = []
-    count = 0
-    for i in lenn2:
-        num10.append(int(i) * (16 ** count))
-        count += 1
-    ans = sum(num10)
-    print('decimal = ', ans)
-
+# xyan831
 
 # decimal to base 2, 8, 16
 def dec_convert(num, base):
-    hexd = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+    # for numbers greater than 10
+    hexd = {'10':'A', '11':'B', '12':'C', '13':'D', '14':'E', '15':'F'}
     con = []
     while num/base != 0:
         con.append(str(num % base))
         num = num // base
     con.reverse()
-    if base == 16:
-        for item in hexd:
-            con = [hexd[item] if x == item else x for x in con]
+    for item in hexd:
+        con = [hexd[item] if x == item else x for x in con]
+    # answer is in string format
     ans = ''.join(con)
     print('base', base, 'number = ', ans)
-
+    return ans
 
 # base 2, 8, 16 to decimal
 def boh_convert(num, base):
-    hexd = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
+    # for numbers greater than 10
+    hexd = {'10':'A', '11':'B', '12':'C', '13':'D', '14':'E', '15':'F'}
     lenn = [item for item in str(num)]
     lenn.reverse()
     con = []
-    if base == 16:
-        for item in hexd:
-            lenn = [item if x == hexd[item] else x for x in lenn]
+    for item in hexd:
+        lenn = [item if x == hexd[item] else x for x in lenn]
     count = 0
     for i in lenn:
         con.append(int(i) * (base ** count))
         count += 1
     ans = sum(con)
     print('decimal = ', ans)
-
+    return ans
 
 # calculate
-nc10_2(13)
-nc10_8(210)
-nc10_16(501)
+a1 = dec_convert(13, 2)
+a2 = dec_convert(210, 8)
+a3 = dec_convert(501, 16)
 
-nc2_10(1101)
-nc8_10(322)
-nc16_10('1F5')
-
-dec_convert(13, 2)
-dec_convert(210, 8)
-dec_convert(501, 16)
-
-boh_convert(1101, 2)
-boh_convert(322, 8)
-boh_convert('1F5', 16)
+a4 = boh_convert(1101, 2)
+a5 = boh_convert(322, 8)
+a6 = boh_convert('1F5', 16)
--- notes_math.py
+++ notes_math.py
@@ -1,100 +1,103 @@
 # notes_math
-# zio800
+# xyan831
 
-from math import *
-from operator import *
+from math import sqrt, radians, sin, cos
+from operator import mul
 import numpy as np
 
+# interpolation
+def interpolation(xi, x, y):
+    yi = y[0] + (xi-x[0])*((y[1]-y[0])/(x[1]-x[0]))
+    print('y = ', yi)
+    return yi
 
 # quadratic equation: (qa)x + (qb)y + (qc) = 0
 def quad_eq(qa, qb, qc):
-    global qe1
-    global qe2
     qe1 = ((-qb) + sqrt((qb ** 2) - (4 * qa * qc))) / (2 * qa)
     qe2 = ((-qb) - sqrt((qb ** 2) - (4 * qa * qc))) / (2 * qa)
+    qe = [qe1, qe2]
     print('quadratic equation (+):\n', qe1)
     print('quadratic equation (-):\n', qe2)
-
+    return qe
 
 # magnitude & unit vector: [vx, vy, vz]
 def mag_univ(vx, vy, vz):
-    global univ
+    global mag
     mag = sqrt(vx**2 + vy**2 + vz**2)
     univ = [vx/mag, vy/mag, vz/mag]
     print('magnitude:\n', mag)
     print('unit vector:\n', univ)
-
+    return univ
 
 # dot & cross product: (v1 * v2), (v1 x v2)
 def dot_cross(v1, v2):
-    dot_pro = sum(list(map(mul, v1, v2)))
-    print('dot product:\n', dot_pro)
+    global dot
+    dot = sum(list(map(mul, v1, v2)))
+    print('dot product:\n', dot)
     x = v1[1]*v2[2] - v2[1]*v1[2]
     y = -(v1[0]*v2[2] - v2[0]*v1[2])
     z = v1[0]*v2[1] - v2[0]*v1[1]
-    cross_pro = [x, y, z]
-    print('cross product:\n', cross_pro)
-
+    cross = [x, y, z]
+    print('cross product:\n', cross)
+    return cross
 
 # matrix solve linear equation
 def mat_lin(na, nb):
-    global lin_eq
     lin_eq = np.linalg.solve(na, nb)
     print('solution:\n', lin_eq)
-
+    return lin_eq
 
 # average and standard deviation
 def avg_sd(lst):
-    global avg
-    global sd
-    s = sum(lst)
-    n = len(lst)
-    avg = s/n
+    avg = sum(lst)/len(lst)
     f = []
     for i in lst:
         f.append((i-avg)**2)
-    sd = (sum(f)/(n-1))**(1/2)
+    sd = (sum(f)/(len(lst)-1))**(1/2)
+    ad = [avg, sd]
     print('average = ', avg)
     print('standard deviation = ', sd)
     print('upper : lower error = ', avg+sd, ':', avg-sd)
-
+    return ad
 
 # decimal to base 2, 8, 16
 def base10(num10):
-    bin_n = bin(num10)
-    oct_n = oct(num10)
-    hex_n = hex(num10)
-    print(int(bin_n, 2), '(binary) = ', bin_n)
-    print(int(oct_n, 8), '(octal) = ', oct_n)
-    print(int(hex_n, 16), '(hexadecimal) = ', hex_n)
-
+    base = [bin(num10), oct(num10), hex(num10)]
+    print(int(base[0], 2), '(binary) = ', base[0])
+    print(int(base[1], 8), '(octal) = ', base[1])
+    print(int(base[2], 16), '(hexadecimal) = ', base[2])
+    return base
 
 # phasor: euler's formula e^(i*theta) = cos(theta) + i*sin(theta)
 def phasor(a, t):
-    global r
-    global i
     th = radians(t)
     r = a*cos(th)
     i = a*sin(th)
+    cn = [r, i]
     print('ans = ', r, '+ j', i)
-
+    return cn
 
 # calculate
-quad_eq(4, 1, -3)
+xi = 0.495
+x = [0.4949, 0.4951]
+y = [2.57, 2.58]
+yi = interpolation(xi, x, y)
 
-mag_univ(-1, 1.5, 2)
+a1 = quad_eq(4, 1, -3)
+
+a2 = mag_univ(-1, 1.5, 2)
 
 va = [-7, 0.4+5.25, 0]
 vb = [-3, -12, 4]
-dot_cross(va, vb)
+a3 = dot_cross(va, vb)
 
 lin_a = np.array([[1, 1.9], [1, 5.8]])
 lin_b = np.array([49.4, 99.3])
-mat_lin(lin_a, lin_b)
+a4 = mat_lin(lin_a, lin_b)
 
 l1 = [520, 512, 515, 522]
-avg_sd(l1)
+a5 = avg_sd(l1)
 
-base10(79)
+a6 = base10(79)
 
-phasor(14.54, 9.90)
+a7 = phasor(14.54, 9.90)
