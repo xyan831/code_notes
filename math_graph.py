5627cad9365fcafb70b15c161531dd8a0efb47c3
Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2021-04-17 17:07:24 -0500

    Add files via upload

--- math_graph.py
+++ math_graph.py
@@ -1,6 +1,7 @@
 # graph
 # xyan831
 from math import sin, cos
+from pylab import pi, logspace, polymul
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,6 +43,26 @@ def squarewave():
     # Display the square wave drawn
     plt.show()
 
-graph()
-squarewave()
+def bodeplot():
+    # denominator and numerator
+    denominator = polymul([1, 0], [1, 16, 100])
+    numerator = [7.5]
+    numerator = polymul(numerator, [0.2, 1])
+    numerator = polymul(numerator, [1, 1])
+    
+    system = signal.lti(numerator, denominator)
+    domain = logspace(-3, 3)
+    omega, magnitude, phase = signal.bode(system, 2*pi*domain)
+    # break frequencies
+    break_frequencies = [1, 5, 10]
+    
+    _, plot = plt.subplots(2, sharex=True)
+    plot[0].semilogx(omega, magnitude)
+    plot[0].vlines(break_frequencies, ymin=min(magnitude), ymax=max(magnitude), colors='C1')
+    plot[0].set_title('Bode Plot')
+    plot[1].semilogx(omega, phase)
+    plot[1].vlines(break_frequencies, ymin=min(magnitude), ymax=max(magnitude), colors='C1')
 
+#graph()
+#squarewave()
+bodeplot()
