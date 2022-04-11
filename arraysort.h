a03b507ad6d4f2a4ceba20f37304261574b596ae
Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-04-11 10:38:57 -0500

    Delete arraysort.h

--- arraysort.h
+++
@@ -1,5 +0,0 @@
-void minmax(int a[], int len, int *min, int *max);
-void multiple(int fac, int start, int end);
-
-void randarray(int min, int max, int rarray[], int size);
-void sortarray(int rarray[], int sarray[], int size);
