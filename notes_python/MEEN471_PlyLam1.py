79e14e21679887926b87ff6b82abe64d844db95f
Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-06-02 11:14:35 -0500

    Add files via upload

--- notes_python/MEEN471_PlyLam1.py
+++ notes_python/MEEN471_PlyLam1.py
@@ -28,95 +28,74 @@ Slc = 217.5*K
 Stt = 5.80*K
 Stc = 35.7*K
 Slts = 9.86*K
-
 # matrix solve linear equation
 def mat_lin(na, nb):
-    lin_eq = np.linalg.solve(na, nb)
-    return lin_eq
+	lin_eq = np.linalg.solve(na, nb)
+	return lin_eq
 # matrix
 def T1(th): # stress local to global
-    T1 = np.array([[cos(th)**2, sin(th)**2, 2*sin(th)*cos(th)],
-                   [sin(th)**2, cos(th)**2, -2*sin(th)*cos(th)],
-                   [-sin(th)*cos(th), sin(th)*cos(th), cos(th)**2-sin(th)**2]])
-    return T1
+	T1 = np.array([[cos(th)**2, sin(th)**2, 2*sin(th)*cos(th)],
+				[sin(th)**2, cos(th)**2, -2*sin(th)*cos(th)],
+				[-sin(th)*cos(th), sin(th)*cos(th), cos(th)**2-sin(th)**2]])
+	return T1
 def T2(th): # strain local to global
-    T2 = np.array([[cos(th)**2, sin(th)**2, sin(th)*cos(th)],
-                   [sin(th)**2, cos(th)**2, -sin(th)*cos(th)],
-                   [-2*sin(th)*cos(th), 2*sin(th)*cos(th), cos(th)**2-sin(th)**2]])
-    return T2
+	T2 = np.array([[cos(th)**2, sin(th)**2, sin(th)*cos(th)],
+				[sin(th)**2, cos(th)**2, -sin(th)*cos(th)],
+				[-2*sin(th)*cos(th), 2*sin(th)*cos(th), cos(th)**2-sin(th)**2]])
+	return T2
 def Q1(E11, E22, G12, v12, v21): # local stress to strain
-    Q1 = np.array([[E11/(1-v12*v21), v12*E22/(1-v12*v21), 0],
-                   [v21*E11/(1-v12*v21), E22/(1-v12*v21), 0],
-                   [0, 0, G12]])
-    return Q1
-def S1(E11, E22, G12, v12, v21): # local strain to stress
-    S1 = np.array([[1/E11, -v12/E11, 0],
-                   [-v21/E22, 1/E22, 0],
-                   [0, 0, 1/G12]])
-    return S1
+	Q1 = np.array([[E11/(1-v12*v21), v12*E22/(1-v12*v21), 0],
+				[v21*E11/(1-v12*v21), E22/(1-v12*v21), 0],
+				[0, 0, G12]])
+	return Q1
 def Q2(Q1, T1, T2): # global stress to strain
-    return np.linalg.inv(T1) @ Q1 @ T2
-def S2(Q2): # global strain to stress
-    return np.linalg.inv(Q2)
-
-def make(Q1, x, y, N, config, pt, let):
-    a = 0
-    b = 0
-    d = 0
-    for i in range(1, N+1):
-        Q = Q2(Q1, T1(radians(config[i-1])), T2(radians(config[i-1])))
-        zk = pt*(i - N/2)
-        zk1 = pt*(i-1 - N/2)
-        a += Q[x][y]*(zk-zk1)
-        b += (1/2)*Q[x][y]*(zk**2-zk1**2)
-        d += (1/3)*Q[x][y]*(zk**3-zk1**3)
-    if let=='A':
-        return a
-    elif let=='B':
-        return b
-    elif let=='D':
-        return d
-
-# matrix
+	return np.linalg.inv(T1) @ Q1 @ T2
+def make(Q1, x, y, N, config, pt):
+	a = 0
+	for i in range(1, N+1):
+		Q = Q2(Q1, T1(radians(config[i-1])), T2(radians(config[i-1])))
+		zk = pt*(i - N/2)
+		zk1 = pt*(i-1 - N/2)
+		a += Q[x][y]*(zk-zk1)
+	return a
 Q1 = Q1(E11, E22, G12, v12, v21)
-
 def matA(Q1, N, bal):
-    A = np.array([[make(Q1, 0, 0, N, config, pt, 'A'), make(Q1, 0, 1, N, config, pt, 'A'), make(Q1, 0, 2, N, config, pt, 'A')],
-                  [make(Q1, 1, 0, N, config, pt, 'A'), make(Q1, 1, 1, N, config, pt, 'A'), make(Q1, 1, 2, N, config, pt, 'A')],
-                  [make(Q1, 2, 0, N, config, pt, 'A'), make(Q1, 2, 1, N, config, pt, 'A'), make(Q1, 2, 2, N, config, pt, 'A')]])
-    A_b = np.array([[make(Q1, 0, 0, N, config, pt, 'A'), make(Q1, 0, 1, N, config, pt, 'A'), 0],
-                    [make(Q1, 1, 0, N, config, pt, 'A'), make(Q1, 1, 1, N, config, pt, 'A'), 0],
-                    [0, 0, make(Q1, 2, 2, N, config, pt, 'A')]]) # balanced
-    if bal == True:
-        return A_b
-    else:
-        return A
+	A = np.array([[make(Q1, 0, 0, N, config, pt, 'A'), make(Q1, 0, 1, N, config, pt, 'A'), make(Q1, 0, 2, N, config, pt, 'A')],
+			   [make(Q1, 1, 0, N, config, pt, 'A'), make(Q1, 1, 1, N, config, pt, 'A'), make(Q1, 1, 2, N, config, pt, 'A')],
+			   [make(Q1, 2, 0, N, config, pt, 'A'), make(Q1, 2, 1, N, config, pt, 'A'), make(Q1, 2, 2, N, config, pt, 'A')]])
+	A_b = np.array([[make(Q1, 0, 0, N, config, pt, 'A'), make(Q1, 0, 1, N, config, pt, 'A'), 0],
+				 [make(Q1, 1, 0, N, config, pt, 'A'), make(Q1, 1, 1, N, config, pt, 'A'), 0],
+				 [0, 0, make(Q1, 2, 2, N, config, pt, 'A')]]) # balanced
+	if bal == True:
+		return A_b
+	else:
+		return A
 
 def matB(Q1, N, sym):
-    B = np.array([[make(Q1, 0, 0, N, config, pt, 'B'), make(Q1, 0, 1, N, config, pt, 'B'), make(Q1, 0, 2, N, config, pt, 'B')],
-                  [make(Q1, 1, 0, N, config, pt, 'B'), make(Q1, 1, 1, N, config, pt, 'B'), make(Q1, 1, 2, N, config, pt, 'B')],
-                  [make(Q1, 2, 0, N, config, pt, 'B'), make(Q1, 2, 1, N, config, pt, 'B'), make(Q1, 2, 2, N, config, pt, 'B')]])
-    B_s = np.array([[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]]) # symmetric
-    if sym == True:
-        return B_s
-    else:
-        return B
+	B = np.array([[make(Q1, 0, 0, N, config, pt, 'B'), make(Q1, 0, 1, N, config, pt, 'B'), make(Q1, 0, 2, N, config, pt, 'B')],
+			   [make(Q1, 1, 0, N, config, pt, 'B'), make(Q1, 1, 1, N, config, pt, 'B'), make(Q1, 1, 2, N, config, pt, 'B')],
+			   [make(Q1, 2, 0, N, config, pt, 'B'), make(Q1, 2, 1, N, config, pt, 'B'), make(Q1, 2, 2, N, config, pt, 'B')]])
+	B_s = np.array([[0, 0, 0],
+				 [0, 0, 0],
+				 [0, 0, 0]]) # symmetric
+	if sym == True:
+		return B_s
+	else:
+		return B
 
 A = matA(Q1, N, True)
 B = matB(Q1, N, True)
 
 D = np.array([[make(Q1, 0, 0, N, config, pt, 'D'), make(Q1, 0, 1, N, config, pt, 'D'), make(Q1, 0, 2, N, config, pt, 'D')],
-              [make(Q1, 1, 0, N, config, pt, 'D'), make(Q1, 1, 1, N, config, pt, 'D'), make(Q1, 1, 2, N, config, pt, 'D')],
-              [make(Q1, 2, 0, N, config, pt, 'D'), make(Q1, 2, 1, N, config, pt, 'D'), make(Q1, 2, 2, N, config, pt, 'D')]])
+			  [make(Q1, 1, 0, N, config, pt, 'D'), make(Q1, 1, 1, N, config, pt, 'D'), make(Q1, 1, 2, N, config, pt, 'D')],
+			  [make(Q1, 2, 0, N, config, pt, 'D'), make(Q1, 2, 1, N, config, pt, 'D'), make(Q1, 2, 2, N, config, pt, 'D')]])
 
 matrix = np.array([[A[0][0], A[0][1], A[0][2], B[0][0], B[0][1], B[0][2]],
-                   [A[1][0], A[1][1], A[1][2], B[1][0], B[1][1], B[1][2]],
-                   [A[2][0], A[2][1], A[2][2], B[2][0], B[2][1], B[2][2]],
-                   [B[0][0], B[0][1], B[0][2], D[0][0], D[0][1], D[0][2]],
-                   [B[1][0], B[1][1], B[1][2], D[1][0], D[1][1], D[1][2]],
-                   [B[2][0], B[2][1], B[2][2], D[2][0], D[2][1], D[2][2]]])
+				   [A[1][0], A[1][1], A[1][2], B[1][0], B[1][1], B[1][2]],
+				   [A[2][0], A[2][1], A[2][2], B[2][0], B[2][1], B[2][2]],
+				   [B[0][0], B[0][1], B[0][2], D[0][0], D[0][1], D[0][2]],
+				   [B[1][0], B[1][1], B[1][2], D[1][0], D[1][1], D[1][2]],
+				   [B[2][0], B[2][1], B[2][2], D[2][0], D[2][1], D[2][2]]])
 
 # laminate
 lam = mat_lin(matrix, np.array([Nxx, Nyy, Nxy, Mxx, Myy, Mxy]))
--- notes_python/MEEN489_stressgraph.py
+++ notes_python/MEEN489_stressgraph.py
@@ -20,10 +20,10 @@ ntxy = np.zeros(len(theta), dtype=float)
 
 # equations
 for i in range(len(theta)):
-    th[i] = radians(theta[i])
-    nsx[i] = ((sx+sy)/2) + (((sx-sy)/2)*cos(2*th[i])) + (txy*sin(2*th[i]))
-    nsy[i] = ((sx+sy)/2) - (((sx-sy)/2)*cos(2*th[i])) - (txy*sin(2*th[i]))
-    ntxy[i] = -(((sx-sy)/2)*sin(2*th[i])) + (txy*cos(2*th[i]))
+	th[i] = radians(theta[i])
+	nsx[i] = ((sx+sy)/2) + (((sx-sy)/2)*cos(2*th[i])) + (txy*sin(2*th[i]))
+	nsy[i] = ((sx+sy)/2) - (((sx-sy)/2)*cos(2*th[i])) - (txy*sin(2*th[i]))
+	ntxy[i] = -(((sx-sy)/2)*sin(2*th[i])) + (txy*cos(2*th[i]))
 
 # graph
 ax = plt.subplot(1, 1, 1)
@@ -36,7 +36,7 @@ plt.title('MEEN 489-507 In Class Work 4/6/2021')
 plt.xlabel('Degrees', fontsize=18)
 plt.ylabel('Stress [MPa]', fontsize=18)
 plt.legend([line1, line2, line3],
-           ['Sx', 'Sy', 'Txy'], fontsize=12,
-           bbox_to_anchor=(1.1, 0.65), loc=2, borderaxespad=0.0)
+		   ['Sx', 'Sy', 'Txy'], fontsize=12,
+		   bbox_to_anchor=(1.1, 0.65), loc=2, borderaxespad=0.0)
 plt.grid()
 plt.show()
