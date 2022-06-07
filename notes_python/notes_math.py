74446d581581bb858c6ca1c9c5166bc5d947f997
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-06-07 11:40:22 -0500

    Add files via upload

--- notes_python/notes_math.py
+++ notes_python/notes_math.py
@@ -9,23 +9,18 @@ import numpy as np
 # interpolation
 def interpolation(xi, x, y):
 	yi = y[0] + (xi-x[0])*((y[1]-y[0])/(x[1]-x[0]))
-	#print('y = ', yi)
 	return yi
 
-# quadratic equation: (qa)x + (qb)y + (qc) = 0
-def quad_eq(qa, qb, qc):
-	qe1 = ((-qb) + sqrt((qb ** 2) - (4 * qa * qc))) / (2 * qa)
-	qe2 = ((-qb) - sqrt((qb ** 2) - (4 * qa * qc))) / (2 * qa)
-	#print('quadratic equation (+):\n', qe1)
-	#print('quadratic equation (-):\n', qe2)
-	return [qe1, qe2]
+# quadratic equation: (a)x + (b)y + (c) = 0
+def quad_eq(a, b, c):
+	q1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+	q2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
+	return [q1, q2]
 
-# magnitude & unit vector: [vx, vy, vz]
-def mag_univ(vx, vy, vz):
-	mag = sqrt(vx**2 + vy**2 + vz**2)
-	univ = [vx/mag, vy/mag, vz/mag]
-	#print('magnitude:\n', mag)
-	#print('unit vector:\n', univ)
+# magnitude & unit vector: [x, y, z]
+def mag_univ(x, y, z):
+	mag = sqrt(x**2 + y**2 + z**2)
+	univ = [x/mag, y/mag, z/mag]
 	return [mag, univ]
 
 # dot & cross product: (v1 * v2), (v1 x v2)
@@ -35,14 +30,11 @@ def dot_cross(v1, v2):
 	y = -(v1[0]*v2[2] - v2[0]*v1[2])
 	z = v1[0]*v2[1] - v2[0]*v1[1]
 	cross = [x, y, z]
-	#print('dot product:\n', dot)
-	#print('cross product:\n', cross)
 	return [dot, cross]
 
 # matrix solve linear equation
 def mat_lin(na, nb):
 	lin_eq = np.linalg.solve(na, nb)
-	#print('solution:\n', lin_eq)
 	return lin_eq
 
 # average and standard deviation
@@ -52,17 +44,11 @@ def avg_sd(lst):
 	for i in lst:
 		f.append((i-avg)**2)
 	sd = (sum(f)/(len(lst)-1))**(1/2)
-	#print('average = ', avg)
-	#print('standard deviation = ', sd)
-	#print('upper : lower error = ', avg+sd, ':', avg-sd)
 	return [avg, sd]
 
 # decimal to base 2, 8, 16
 def base10(num10):
 	base = [bin(num10), oct(num10), hex(num10)]
-	#print(int(base[0], 2), '(binary) = ', base[0])
-	#print(int(base[1], 8), '(octal) = ', base[1])
-	#print(int(base[2], 16), '(hexadecimal) = ', base[2])
 	return base
 
 # phasor: euler's formula e^(i*theta) = cos(theta) + i*sin(theta)
@@ -71,7 +57,6 @@ def phasor(a, t):
 	r = a*cos(th)
 	i = a*sin(th)
 	cn = [r, i]
-	#print('ans = ', r, '+ j', i)
 	return cn
 
 # calculate
@@ -79,20 +64,28 @@ xi = 0.495
 x = [0.4949, 0.4951]
 y = [2.57, 2.58]
 yi = interpolation(xi, x, y)
+print('interpolation y:', yi)
 
 a1 = quad_eq(4, 1, -3)
+print('quadratic eq:', a1)
 a2 = mag_univ(-1, 1.5, 2)
+print('magnitude:', a2[0], '\nunit vector:', a2[1])
 
 va = [-7, 0.4+5.25, 0]
 vb = [-3, -12, 4]
 a3 = dot_cross(va, vb)
+print('dot:', a3[0], '\ncross', a3[1])
 
-lin_a = np.array([[1, 1.9], [1, 5.8]])
+lin_a = np.array([[1, 1.9],
+				  [1, 5.8]])
 lin_b = np.array([49.4, 99.3])
 a4 = mat_lin(lin_a, lin_b)
+print('matrix lin solution:', a4)
 
-l1 = [520, 512, 515, 522]
-a5 = avg_sd(l1)
-
+a5 = avg_sd([520, 512, 515, 522])
+print('avg:', a5[0], '\nstandard dev:', a5[1])
+print('upper : lower error:', a5[0]+a5[1], ":", a5[0]-a5[1])
 a6 = base10(79)
+print('binary:', a6[0], '\noctal:', a6[1], '\nhexadecimal:', a6[2])
 a7 = phasor(14.54, 9.90)
+print('phasor:', a7[0], '+ j', a7[1])
