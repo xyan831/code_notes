dc63d2c0a7b934001f7ac4c8c2cfd04d8006eb59
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-06-09 23:01:07 -0500

    Add files via upload

--- notes_python/gacha.py
+++ notes_python/gacha.py
@@ -1,35 +1,34 @@
 # gacha
 # xyan831
 
+# import module
 import random
-
-# probablilty
-gachaType = ['SSR', 'SR', 'R', 'N']
-gachaWeight = [4, 10, 20, 10]
 
 # pool
 SSR = ['SSR_one', 'SSR_two', 'SSR_three']
-SR = ['SR_one', 'SR_two', 'SR_three']
-R = ['R_one', 'R_two', 'R_three']
-N = ['N_one', 'N_two', 'N_three']
+_SR = ['SR_one', 'SR_two', 'SR_three']
+__R = ['R_one', 'R_two', 'R_three']
+__N = ['N_one', 'N_two', 'N_three']
+
+# probablilty
+Weight = [4, 20, 50, 30]
+
+# create master list
+mlst = [['SSR', SSR],
+		[' SR', _SR],
+		['  R', __R],
+		['  N', __N]]
 
 # pull from pool
 def gachaGet(Type, Lst):
-  print(f'{Type}：  {random.choice(Lst)}')
+	print(f'{Type}：  {random.choice(Lst)}')
 
 # number of pulls
 def gacha(num):
-  print('你抽到了：')
-  gachaLst = random.choices(gachaType, weights=gachaWeight, k=num)
-  for i in gachaLst:
-    if (i=='SSR'):
-      gachaGet('SSR', SSR)
-    elif (i=='SR'):
-      gachaGet(' SR', SR)
-    elif (i=='R'):
-      gachaGet('  R', R)
-    elif (i=='N'):
-      gachaGet('  N', N)
+	print('你抽到了：')
+	gachalst = random.choices(mlst, weights=Weight, k=num)
+	for i in gachalst:
+		gachaGet(i[0], i[1])
 
 # pull gacha
 gacha(10)
--- notes_python/gacha_writeCSV.py
+++ notes_python/gacha_writeCSV.py
@@ -4,21 +4,20 @@
 # import module
 import pandas as pd
 
-# probablilty
-Type = ['SSR', ' SR', '  R', '  N']
-Weight = [1, 20, 50, 29]
-
 # pool
 SSR = ['SSR_one', 'SSR_two', 'SSR_three']
-SR = ['SR_one', 'SR_two', 'SR_three']
-R = ['R_one', 'R_two', 'R_three']
-N = ['N_one', 'N_two', 'N_three']
+_SR = ['SR_one', 'SR_two', 'SR_three']
+__R = ['R_one', 'R_two', 'R_three']
+__N= ['N_one', 'N_two', 'N_three']
+
+# probablilty
+Weight = [1, 20, 50, 29]
 
 # create master list
-mlst = [[Type[0], Weight[0], SSR],
-		[Type[1], Weight[1], SR],
-		[Type[2], Weight[2], R],
-		[Type[3], Weight[3], N]]
+mlst = [['SSR', Weight[0], SSR],
+		[' SR', Weight[1], _SR],
+		['  R', Weight[2], __R],
+		['  N', Weight[3], __N]]
 
 # create dataframe
 gacha = pd.DataFrame(mlst, columns=['Type', 'Weight', 'Gacha'])
