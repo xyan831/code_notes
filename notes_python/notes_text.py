80b1ce0f65b8970c123b38442fb97869e2bd7cb4
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-07-07 17:11:20 -0500

    Add files via upload

--- notes_python/notes_text.py
+++ notes_python/notes_text.py
@@ -5,6 +5,7 @@
 import os
 import re
 
+# class for files
 class File:
 	# get all files from directory based on file type
 	def TYPE(filetyp='.py', filedir=os.getcwd()):
@@ -13,34 +14,25 @@ class File:
 			if file.endswith(filetyp):
 				filelst.append(file)
 		return filelst
+	# rename file
 	def RENAME(oldname, newname, directory=os.getcwd()):
 		olddir = directory+'\\'+oldname
 		newdir = directory+'\\'+newname
 		os.rename(olddir, newdir)
+	# remove file
 	def REMOVE(lst, filedir=os.getcwd()):
 		for i in lst:
 			os.remove(filedir+'\\'+i)
 
+# class for text
 class Text:
+	# find text between 2 markers
 	def FIND(mark, txt):
 		pat = re.compile(mark[0]+'(.*?)'+mark[1], re.S)
 		lst = pat.findall(txt)
 		return lst
+	# delete characters in text
 	def DEL(lst, txt):
 		for item in lst:
 			txt = txt.replace(item, '')
 		return txt
-
-# test
-filelst = File.TYPE()
-print(filelst)
-
-text1 = 'asdf=5;iwantthis123jasd asdf=5;iwantthis123jasd'
-mark = ['asdf=5;', '123jasd']
-txtlst = Text.FIND(mark, text1)
-print(txtlst)
-
-chara = [' ', '\n', '\t']
-text2 = 'yo wasupo \n \t sfdgsdfg'
-text = Text.DEL(chara, text2)
-print(text)
