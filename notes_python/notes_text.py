a2fbf283c18624e061e7bb6ad4785711e03994ef
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-07-07 14:59:49 -0500

    Add files via upload

--- notes_python/notes_text.py
+++ notes_python/notes_text.py
@@ -5,42 +5,42 @@
 import os
 import re
 
-# get all files from directory based on file type
-def file_typget(filetyp='.py', filedir=os.getcwd()):
-	filelst = []
-	for file in os.listdir(filedir):
-		if file.endswith(filetyp):
-			filelst.append(file)
-	return filelst
+class File:
+	# get all files from directory based on file type
+	def TYPE(filetyp='.py', filedir=os.getcwd()):
+		filelst = []
+		for file in os.listdir(filedir):
+			if file.endswith(filetyp):
+				filelst.append(file)
+		return filelst
+	def RENAME(oldname, newname, directory=os.getcwd()):
+		olddir = directory+'\\'+oldname
+		newdir = directory+'\\'+newname
+		os.rename(olddir, newdir)
+	def REMOVE(lst, filedir=os.getcwd()):
+		for i in lst:
+			os.remove(filedir+'\\'+i)
 
-# rename file
-def file_rename(oldname, newname, directory=os.getcwd()):
-	olddir = directory+'\\'+oldname
-	newdir = directory+'\\'+newname
-	os.rename(olddir, newdir)
-
-# find text between 2 marks [start, end]
-def find(mark, txt):
-	pat = re.compile(mark[0]+'(.*?)'+mark[1], re.S)
-	lst = pat.findall(txt)
-	return lst
-
-# delete characters
-def delchar(lst, txt):
-	for item in lst:
-		txt = txt.replace(item, '')
-	return txt
+class Text:
+	def FIND(mark, txt):
+		pat = re.compile(mark[0]+'(.*?)'+mark[1], re.S)
+		lst = pat.findall(txt)
+		return lst
+	def DEL(lst, txt):
+		for item in lst:
+			txt = txt.replace(item, '')
+		return txt
 
 # test
-filelst = file_typget('.txt')
+filelst = File.TYPE()
 print(filelst)
 
 text1 = 'asdf=5;iwantthis123jasd asdf=5;iwantthis123jasd'
 mark = ['asdf=5;', '123jasd']
-txtlst = find(mark, text1)
+txtlst = Text.FIND(mark, text1)
 print(txtlst)
 
 chara = [' ', '\n', '\t']
 text2 = 'yo wasupo \n \t sfdgsdfg'
-text = delchar(chara, text2)
+text = Text.DEL(chara, text2)
 print(text)
