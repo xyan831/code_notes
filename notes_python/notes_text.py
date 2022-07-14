103038dfbe6fe0604a6572b72a0d3a491fa2af5f
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-07-13 19:43:16 -0500

    Add files via upload

--- notes_python/notes_text.py
+++ notes_python/notes_text.py
@@ -5,35 +5,48 @@
 import os
 import re
 
-# class for files
-class File:
-	# get files from directory based on file type
-	def TYPE(typlst=['.py'], filedir=os.getcwd()):
-		filelst = []
-		for file in os.listdir(filedir):
-			for typ in typlst:
-				if file.endswith(typ):
-					filelst.append(file)
-		return filelst
-	# rename file in same directory
-	def RENAME(oldname, newname, directory=os.getcwd()):
-		olddir = directory+'\\'+oldname
-		newdir = directory+'\\'+newname
-		os.rename(olddir, newdir)
-	# remove files from same directory
-	def REMOVE(lst, filedir=os.getcwd()):
-		for i in lst:
-			os.remove(filedir+'\\'+i)
+# file functions
 
-# class for text
-class Text:
-	# find text between 2 markers
-	def FIND(mark, txt):
-		pat = re.compile(mark[0]+'(.*?)'+mark[1], re.S)
-		lst = pat.findall(txt)
-		return lst
-	# delete characters in text
-	def DEL(lst, txt):
-		for item in lst:
-			txt = txt.replace(item, '')
-		return txt
+# get files from directory based on file type
+def getFiles(typlst, directory):
+	filelst = []
+	for file in os.listdir(directory):
+		for typ in typlst:
+			if file.endswith(typ):
+				filelst.append(file)
+	return filelst
+
+# rename file in same directory
+def Rename(oldname, newname, directory):
+	olddir = directory+'\\'+oldname
+	newdir = directory+'\\'+newname
+	os.rename(olddir, newdir)
+
+# remove files from same directory
+def Remove(lst, directory):
+	for i in lst:
+		os.remove(directory+'\\'+i)
+
+# clean out directory
+def cleanout(directory):
+	files = os.listdir(directory)
+	for item in files:
+		if ('.' not in item):
+			cleanout(directory+'\\'+item)
+			os.rmdir(directory+'\\'+item)
+		else:
+			os.remove(directory+'\\'+item)	
+
+# text functions
+
+# find text between 2 markers
+def FIND(mark, txt):
+	pat = re.compile(mark[0]+'(.*?)'+mark[1], re.S)
+	lst = pat.findall(txt)
+	return lst
+
+# delete characters in text
+def DEL(lst, txt):
+	for item in lst:
+		txt = txt.replace(item, '')
+	return txt
