7c47ef1e4b98d30e3c7d155c08246d6acae2098c
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2022-08-01 16:23:00 -0500

    Add files via upload

--- notes_python/gacha.py
+++ notes_python/gacha.py
@@ -9,31 +9,33 @@ class Gacha:
 	def __init__(self, pool, weight):
 		self.pool = pool
 		self.weight = weight
+		self.own = {}
 	# pull gacha
 	def PULL(self, num):
-		# pull from pool
-		def GET(Type, Lst):
-			print(f'{Type}：  {random.choice(Lst)}')
 		# start gacha
 		print('你抽到了：')
 		# choose pools
 		gachalst = random.choices(self.pool, weights=self.weight, k=num)
-		# pull gachas
+		# pull from pool
 		for i in gachalst:
-			GET(i[0], i[1])
-
+			char = random.choice(i[1])
+			print(f"{i[0]}：  {char}")
+			if (char in self.own):
+				self.own[char] += 1
+			else:
+				self.own[char] = 1
 '''
 # pool
-SSR = ['SSR_ein', 'SSR_zwei', 'SSR_drei']
-_SR = ['SR_ein', 'SR_zwei', 'SR_drei']
-__R = ['R_ein', 'R_zwei', 'R_drei']
-__N = ['N_ein', 'N_zwei', 'N_drei']
+ssr = ['SSR_ein', 'SSR_zwei', 'SSR_drei']
+sr = ['SR_ein', 'SR_zwei', 'SR_drei']
+r = ['R_ein', 'R_zwei', 'R_drei']
+n = ['N_ein', 'N_zwei', 'N_drei']
 
 # create pool list
-pool = [['SSR', SSR],
-		[' SR', _SR],
-		['  R', __R],
-		['  N', __N]]
+pool = [['SSR', ssr],
+		[' SR', sr],
+		['  R', r],
+		['  N', n]]
 
 # probablilty
 weight = [4, 20, 50, 30]
